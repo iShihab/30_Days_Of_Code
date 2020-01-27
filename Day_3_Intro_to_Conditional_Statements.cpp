81bfd867763970c1915a4456b84a643fa145b9ec
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-27 18:42:47 +0600

    Update Day_3_Intro_to_Conditional_Statements.cpp

--- Day_3_Intro_to_Conditional_Statements.cpp
+++ Day_3_Intro_to_Conditional_Statements.cpp
@@ -7,6 +7,22 @@ int main()
     int N;
     cin >> N;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (N % 2 != 0)
+    {
+        cout << "Weird" << endl;
+    }
+    else if (N >= 2 && N <= 5)
+    {
+        cout << "Not Wird" << endl;
+    }
+    else if (N >= 6 && N <= 20)
+    {
+        cout << "Weird" << endl;
+    }
+    else if (N % 2 == 0 && N > 20)
+    {
+        cout << "Not Wird" << endl;
+    }
 
     return 0;
 }
