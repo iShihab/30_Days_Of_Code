cb8f97326c611ac973785d659cdf534d2dc11e5e
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-29 09:32:29 +0600

    Update Day_4_Class_vs_Instance.cpp

--- Day_4_Class_vs_Instance.cpp
+++ Day_4_Class_vs_Instance.cpp
@@ -42,3 +42,24 @@ void Person::yearPasses()
 {
     age++;
 }
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    int age;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> age;
+        Person p(age);
+        p.amIOld();
+        for (int j = 0; j < 3; i++)
+        {
+            p.yearPasses();
+        }
+        p.amIOld();
+        cout << "\n";
+    }
+
+    return 0;
+}
